fcca403bb31ea3a7e1ee9b511eb6453342dddbe4
unknown <didierdemaeyer@telenet.be>
2014-09-23 11:47:49 +0200

    oplossing opdracht comments

---
+++ oplossing-opdracht-comments/comments.php
@@ -0,0 +1,16 @@
+<?php
+	$naam = 'Didier De Maeyer'
+	#Didier De maeyer
+	//didier.demaeyer@student.kdg.be
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Oplossing opdracht comments</title>
+</head>
+<body>
+	<p><?= $naam ?></p>
+</body>
+</html>
