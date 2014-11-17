2fe4209d4c75738a3a23b82eab0804ad952599ef
Didier De Maeyer <didierdemaeyer@telenet.be>
2014-11-17 21:02:21 +0100

    update periodeopdracht-01-todo

--- periodeopdracht-01-todo/index.php
+++ periodeopdracht-01-todo/index.php
@@ -4,9 +4,9 @@
 	if (isset($_POST['addTodo'])) {
 		if ($_POST['addTodo'] != "") {
 			$_SESSION['todo'][] = $_POST['description'];
-
-			unset($_POST['addTodo']);
 		}
+
+		header("location: index.php");	//Zorgt ervoor dat wanneer je de pagina refreshed het element niet opnieuw wordt toegevoegd
 	}
 
 
@@ -15,11 +15,15 @@
 	if (isset($_POST['toggleTodo'])) {
 		$_SESSION['done'][] = $_SESSION['todo'][$_POST['toggleTodo']];
 		unset($_SESSION['todo'][$_POST['toggleTodo']]);
+
+		header("location: index.php");	//Zorgt ervoor dat wanneer je de pagina refreshed het element niet opnieuw wordt verplaatst wat een foutmelding geeft
 	}
 
 	if (isset($_POST['toggleDone'])) {
 		$_SESSION['todo'][] = $_SESSION['done'][$_POST['toggleDone']];
 		unset($_SESSION['done'][$_POST['toggleDone']]);
+
+		header("location: index.php");	//Zorgt ervoor dat wanneer je de pagina refreshed het element niet opnieuw wordt verplaatst wat een foutmelding geeft
 	}
 
 
@@ -32,26 +36,6 @@
 	if (isset($_POST['deleteDone'])) {
 		unset( $_SESSION['done'][$_POST['deleteDone']] );
 	}
-
-
-	// //Load the HTML page
-	// $html = file_get_contents('index.php');
-	// //Create a new DOM document
-	// $dom = new DOMDocument;
-	 
-	// //Parse the HTML. The @ is used to suppress any parsing errors
-	// //that will be thrown if the $html string isn't valid XHTML.
-	// @$dom->loadHTML($html);
-	 
-	// //Get all links. You could also use any other tag name here,
-	// //like 'img' or 'table', to extract other tags.
-	// $links = $dom->getElementsByTagName('button');
-		
-	// //Iterate over the extracted links and display their URLs
-	// foreach ($links as $link){
- //    //Extract and show the "href" attribute. 
- //    echo $link->getAttribute('class'), '<br>';
-	// }
 
  ?>
 
@@ -104,27 +88,28 @@
 				
 				<h2>Done and done!</h2>
 				
-				<?php if($_SESSION['done'] != null): ?>
+				<?php if (isset( $_SESSION['done'] )): ?>
+					<?php if($_SESSION['done'] != null): ?>
 
-					<ul>
-						<?php if (isset( $_SESSION['done'] )): ?>
-							<?php foreach($_SESSION['done'] as $key => $done): ?>
-								<li>
-									<form action="index.php" method="POST">
-										<button title="Status wijzigen" name="toggleDone" value="<?= $key ?>" class="status not-done"> <?= $done ?> </button>
-										<button title="Verwijderen" name="deleteDone" value="<?= $key ?>">Verwijder</button>
-									</form>
-							</li>
-							<?php endforeach ?>
-						<?php endif ?>			
-					</ul>
+						<ul>
+								<?php foreach($_SESSION['done'] as $key => $done): ?>
+									<li>
+										<form action="index.php" method="POST">
+											<button title="Status wijzigen" name="toggleDone" value="<?= $key ?>" class="status done"> <?= $done ?> </button>
+											<button title="Verwijderen" name="deleteDone" value="<?= $key ?>">Verwijder</button>
+										</form>
+								</li>
+								<?php endforeach ?>		
+						</ul>
 
-				<?php else: ?>
+					<?php else: ?>
+						
+						<p>Werk aan de winkel...</p>
 					
-					<p>Werk aan de winkel...</p>
+					<?php endif ?>
 				
-				<?php endif ?>
-					
+				<?php endif ?>	
+			
 			<?php endif ?>
 								
 			
