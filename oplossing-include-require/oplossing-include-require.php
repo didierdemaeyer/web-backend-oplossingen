dcbe4c19c08a1f840a7420e599f378c284be7dc1
Didier De Maeyer <didierdemaeyer@telenet.be>
2014-11-24 22:02:36 +0100

    oplossing-include-require

---
+++ oplossing-include-require/oplossing-include-require.php
@@ -0,0 +1,34 @@
+<?php 
+	$artikels = array(
+
+    array(
+        'title' => "PHP 8.4 gereleased",
+        'text' => "De nieuwste versie van PHP bevat uitbreidingen op oa. mind control-facades, in-browser 3d projecties en de API voor drones. ",
+        'tags' => array(
+            0 => "new",
+            1 => "PHP 8.4"
+            )
+    ),
+    array(
+        'title' => "Zymphovel framework",
+        'text' => "Wordt Zymphovel het nieuwste PHP-framework dat de wereld in een sneltempo zal veroveren? ",
+        'tags' => array(
+            0 => "frameworks"
+            )
+    ),
+    array(
+        'title' => "Rasmus Lerdorf vermist",
+        'text' => "De geestelijke vader van PHP Rasmus Lerdorf is sinds vorige week vermist nadat hij met zijn privé-jet richting Ibiza vertrok. ",
+        'tags' => array(
+            0 => "bizar",
+            1 => "rasmus lerdorf",
+            2 => "feestneus"
+            )
+    )
+	);
+
+	require_once "view/header-partial.html";
+	require_once "view/body-partial.html";
+	require_once "view/footer-partial.html";
+
+ ?>
