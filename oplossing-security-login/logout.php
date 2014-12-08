01c074416916c00ff8322877c8448931f01f03bc
Didier De Maeyer <didierdemaeyer@telenet.be>
2014-12-08 22:25:08 +0100

    update oplossing-security-login/logout.php

--- oplossing-security-login/logout.php
+++ oplossing-security-login/logout.php
@@ -2,11 +2,14 @@
 
 	session_start();
 
+	if ( isset($_COOKIE['login']) ) {
+
 	$_SESSION['notification']['type'] = "succes";
 	$_SESSION['notification']['message'] = "U bent uitgelogd. Tot de volgende keer";
 
 	setcookie('login', "", time() - 99999999);
-
+	}
+	
 	header('location: login-form.php');
 	
  ?>
